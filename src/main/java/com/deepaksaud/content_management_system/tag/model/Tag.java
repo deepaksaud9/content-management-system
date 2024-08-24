8a524bf973ced4d437903815b18c563389355bd4
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-08-24 11:57:24 +0545

    feature | tag : add tag model and repository

---
+++ src/main/java/com/deepaksaud/content_management_system/tag/model/Tag.java
@@ -0,0 +1,23 @@
+package com.deepaksaud.content_management_system.tag.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tagId;
+    private String name;
+}
