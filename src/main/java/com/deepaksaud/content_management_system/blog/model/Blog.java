2d7a711dd68d9dd7ec6211b558460a26247d72ad
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-08-24 22:43:16 +0545

    Feature | blog : add blog model and blogrepository

---
+++ src/main/java/com/deepaksaud/content_management_system/blog/model/Blog.java
@@ -0,0 +1,41 @@
+package com.deepaksaud.content_management_system.blog.model;
+import com.deepaksaud.content_management_system.category.model.Category;
+import com.deepaksaud.content_management_system.tag.model.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long blogId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 5000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "blog_tags",
+            joinColumns = @JoinColumn(name = "blog_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
+
+
+}
