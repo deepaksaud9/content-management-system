cdd5f961a25c171d43c2aa528097f69c068de90f
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-08-25 00:30:39 +0545

    Feature | Blog : add createBlogForm

--- src/main/java/com/deepaksaud/content_management_system/blog/controller/BlogController.java
+++ src/main/java/com/deepaksaud/content_management_system/blog/controller/BlogController.java
@@ -2,11 +2,11 @@ package com.deepaksaud.content_management_system.blog.controller;
 
 import com.deepaksaud.content_management_system.blog.model.Blog;
 import com.deepaksaud.content_management_system.blog.service.BlogService;
+import com.deepaksaud.content_management_system.category.service.CategoryService;
+import com.deepaksaud.content_management_system.tag.service.TagService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -15,9 +15,14 @@ import java.util.Set;
 public class BlogController {
 
     private final BlogService blogService;
+    private final CategoryService categoryService;
 
-    public BlogController(BlogService blogService) {
+    private final TagService tagService;
+
+    public BlogController(BlogService blogService, CategoryService categoryService, TagService tagService) {
         this.blogService = blogService;
+        this.categoryService = categoryService;
+        this.tagService = tagService;
     }
 
 
@@ -29,5 +34,12 @@ public class BlogController {
         blogService.createBlog(blog, categoryId, tagIds);
         return "redirect:/blogs";
     }
-    
+
+    @GetMapping("/create")
+    public String createBlogForm(Model model) {
+        model.addAttribute("blog", new Blog());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("tags", tagService.getAllTags());
+        return "blogs/create";
+    }
 }
